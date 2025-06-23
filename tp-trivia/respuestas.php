d5e58f202027b91e8d551fd1bc793c692e1faf0e
malenluna <malenluna302@gmail.com>
2025-06-23 00:28:14 -0300

    respuestas.php

---
+++ tp-trivia/respuestas.php
@@ -0,0 +1,15 @@
+<?php
+$respuestas_correctas = [
+    'pregunta1' => 'Hawkins',
+    'pregunta2' => 'Jim Hopper',
+    'pregunta3' => 'Once',
+    'pregunta4' => 'Demogorgon',
+    'pregunta5' => '1983',
+    'pregunta6' => 'Stumptown',
+    'pregunta7' => 'Dustin Henderson',
+    'pregunta8' => 'Will Byers',
+    'pregunta9' => 'Experimentos con sujetos con poderes psíquicos',
+    'pregunta10' => 'El Mundo del Revés',
+    'pregunta11' => ['Mike', 'Lucas', 'Dustin', 'Will'],
+];
+?>
